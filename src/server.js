e8eb767e5f8b6dbca28a99fe05e2d48ae04c5d70
vinicius rosa <vinicius.rosa00@gmail.com>
2019-10-23 15:31:13 -0300

    files route

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const path = require('path');
 
 const app = express();
 
@@ -19,6 +20,7 @@ mongoose.connect( process.env.MONGODB, {
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')))
 app.use(routes);
 
 app.listen(process.env.PORT, (req, res) => {
